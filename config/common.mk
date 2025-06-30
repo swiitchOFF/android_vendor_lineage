e9b9deeb29841a4a2ab042db34c3d35712a8c6b2
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-07-01 05:14:46 +0800

    common.mk: add blur support
    Change-Id: Ie503986ef2642dbd474bfa5bee8d67688788d544

--- config/common.mk
+++ config/common.mk
@@ -233,6 +233,18 @@ endif
 # Audio files
 $(call inherit-product, vendor/lineage/audio/audio.mk)
 
+# Enable blur
+TARGET_ENABLE_BLUR ?= true
+ifeq ($(TARGET_ENABLE_BLUR),true)
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.custom.blur.enable=true
+else
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.custom.blur.enable=false
+endif
+
+PRODUCT_SYSTEM_PROPERTIES += ro.surface_flinger.supports_background_blur=1
+
 # SetupWizard
 PRODUCT_PRODUCT_PROPERTIES += \
     setupwizard.theme=glif_v4 \
