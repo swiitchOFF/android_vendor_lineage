27a960a123116e9f142a5e78791a9db8c2bb45d3
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-07-01 05:43:25 +0800

    [Optimization] config: reduce memory footprint
    Change-Id: Ifc84bbd8e4790ff0dab33368d9ac45158e7c47fc
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- config/common.mk
+++ config/common.mk
@@ -1,6 +1,7 @@
 # Allow vendor/extra to override any property by setting it first
 $(call inherit-product-if-exists, vendor/extra/product.mk)
 $(call inherit-product-if-exists, vendor/addons/config.mk)
+$(call inherit-product, vendor/lineage/config/defaults_common.mk)
 include vendor/lineage/audio/config.mk
 
 # Allow vendor prebuilt repos to exclude themselves from bp scanning
---
+++ config/defaults_common.mk
@@ -0,0 +1,30 @@
+# Speed profile services and wifi-service to reduce RAM and storage.
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+
+# Do not generate libartd.
+PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
+
+# Strip the local variable table and the local variable type table to reduce
+# the size of the system image. This has no bearing on stack traces, but will
+# leave less information available via JDWP.
+PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
+
+# Always preopt extracted APKs to prevent extracting out of the APK for gms
+# modules.
+PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
+
+# Use a profile based boot image for this device. Low ram optimized taken from atv devices.
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+PRODUCT_COPY_FILES += vendor/lineage/product/lowram_boot_profiles/preloaded-classes:system/etc/preloaded-classes
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := vendor/lineage/product/lowram_boot_profiles/boot-image-profile.txt
+
+PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
+system/etc/preloaded-classes.txt
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.madvise.vdexfile.size=31457280\
+    dalvik.vm.madvise.odexfile.size=31457280\
+    dalvik.vm.madvise.artfile.size=0
+
+TARGET_SYSTEM_PROP += \
+    vendor/lineage/config/defaults_common.prop
