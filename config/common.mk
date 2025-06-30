b26220144eedd0a01c41cc92b469d915e8ad3088
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-07-01 05:57:00 +0800

    [System] config: disable system enforcements
    Change-Id: Ic06db5ff5d9d710d0e75719e8c88fb3f166e3034

--- config/common.mk
+++ config/common.mk
@@ -97,10 +97,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/lineage/config/permissions/org.lineageos.android.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/org.lineageos.android.xml
 
-# Enforce privapp-permissions whitelist
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.control_privapp_permissions=enforce
-
 ifneq ($(TARGET_DISABLE_LINEAGE_SDK), true)
 # Lineage SDK
 include vendor/lineage/config/lineage_sdk_common.mk
